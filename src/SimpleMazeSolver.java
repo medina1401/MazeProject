05a9f894c51bdf9585251767efc4eb952ebdf528
medina1401 <satimbayyy@gmail.com>
2025-04-12 13:10:17 +0600

    created project

--- src/SimpleMazeSolver.java
+++ src/SimpleMazeSolver.java
@@ -1,5 +1,19 @@
-public class SimpleMazeSolver {
-    public static void main(String[] args) {
+import java.util.*;
 
+public class SimpleMazeSolver {
+
+    // Символы для отображения лабиринта
+    static final char WALL = '#';
+    static final char PATH = ' ';
+    static final char START = 'S';
+    static final char EXIT = 'E';
+    static final char VISITED = '.';
+
+    // Размеры лабиринта
+    static final int ROWS = 11;
+    static final int COLS = 21;
+
+    public static void main(String[] args) {
+        // Лабиринт еще не реализован
     }
 }
